f3ff84e222873164a7c39545b3a7baac4a68adc5
Filip <Fpetersson96@gmail.com>
2018-02-26 00:47:24 +0100

    updated with newest demo of portfolio

--- templates/footer.php
+++
@@ -1,4 +0,0 @@
-<!-- <script src="./scripts/particles.min.js"></script>
-<script src="./scripts/particles.js" charset="utf-8"></script> -->
-<script src="./scripts/main.js" charset="utf-8"></script>
-</html>
